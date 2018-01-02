68ad4463f849469447eb4235d6b26a6b54a77c73
alpha18603074401@gmail.com <xiwang1992>
2018-01-02 14:25:29 +0800

    1.集成了消息中间件ActiveMQ 2.集成了Mail

---
+++ src/main/java/com/fan/framework/config/ActiveMQConfig.java
@@ -0,0 +1,16 @@
+package com.fan.framework.config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author:fanwenlong
+ * @date:2017-12-29 20:50:19
+ * @E-mail:alpha18603074401@gmail.com
+ * @mobile:186-0307-4401
+ * @description:activeMQ的上下文
+ * @detail:
+ */
+public class ActiveMQConfig {
+    private static final Logger logger = LoggerFactory.getLogger(ActiveMQConfig.class);
+}
---
+++ src/main/java/com/fan/framework/config/MailConfig.java
@@ -0,0 +1,16 @@
+package com.fan.framework.config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author:fanwenlong
+ * @date:2018-01-02 14:24:53
+ * @E-mail:alpha18603074401@gmail.com
+ * @mobile:186-0307-4401
+ * @description:
+ * @detail:
+ */
+public class MailConfig {
+    private static final Logger logger = LoggerFactory.getLogger(MailConfig.class);
+}
